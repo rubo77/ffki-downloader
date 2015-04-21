f5a2ec691035dc2892e92c957d0404d47a6a716c
do9xe <h.l.lueth@gmail.com>
2015-04-21 22:56:20 +0200

    update for gluon 2015.1 experimental

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 
     $('#download-form').submit(function( event ) {
@@ -7,6 +6,7 @@ $(document).ready(function() {
         var type = '',
             fileExtension = '',
             fileEnding = '.bin',
+            site_code = 'ffki',
             router;
         
         router = $('#download-form-router').val();
@@ -14,11 +14,9 @@ $(document).ready(function() {
         switch ($('#download-form-type').val()) {
             case '0':
                 type = 'factory';
-                /* there will be more than a *.bin as a filending
-                if (router == 'netgear-wndr3700' || router == 'netgear-wndr3700' || router == 'netgear-wndr3800'){
+                if (router == "netgear-wndr3700" || router == "netgear-wndr3700" || router == "netgear-wndr3800"){
                     fileEnding = '.img'
                 }
-                */
                 break;
             case '1':
                 type = 'sysupgrade';
@@ -31,24 +29,34 @@ $(document).ready(function() {
             switch ($('#branch').val()) {
                 case '0':
                     branchdir = 'stable';
-                    vnumber = '0.6-';
+                    vnumber = '0.6';
                     break;
                 case '1':
                     branchdir = 'experimental';
-                    vnumber = '0.6~beta20150104-';
+                    vnumber = '0.7-exp150420';
                     break;
                 default:
                     branchdir = 'stable';
-                    vnumber = '0.6-';
+                    vnumber = '0.6';
             }
 
-        
-
-        if(router === '-1') {
-            window.alert('Bitte wähle eine Router aus.');
-        } else {
-            window.location.href = '/firmware/'+branchdir+'/'+type+'/gluon-ffki-'+vnumber+router+fileExtension+fileEnding;
-        }
+	if ((router == "netgear-wndr3700" || router == "netgear-wndr3700" || router == "netgear-wndr3800" || 
+        router == "buffalo-wzr-hp-ag300h-wzr-600dhp" || router == "buffalo-wzr-hp-g450h" || 
+        router == "d-link-dir-615-rev-c1" || router == "gl-inet-6408a-v1" || router == "gl-inet-6416a-v1" || 
+        router == "tp-link-tl-mr3220-v2" || router == "tp-link-tl-wa701n-nd-v1" || 
+        router == "tp-link-tl-wa860re-v1" || router == "tp-link-tl-wa901n-nd-v3" || 
+        router == "tp-link-tl-wr2543n-nd-v1" || router == "tp-link-tl-wr743n-nd-v1" || 
+        router == "tp-link-tl-wr743n-nd-v2" || router == "tp-link-tl-wr941n-nd-v5" || 
+        router == "ubiquiti-loco-m-xw" || router == "ubiquiti-nanostation-m-xw" || 
+        router == "ubiquiti-unifi-ap-pro") && (vnumber == '0.6'))
+    {
+        window.alert('Der ausgewählte Router ist in dieser Version noch nicht verfügbar!');
+    }
+	else if(router === '-1') {
+        window.alert('Bitte wähle eine Router aus.');
+    } else {
+        window.location.href = '/firmware/'+branchdir+'/'+type+'/gluon-'+site_code+'-'+vnumber+'-'+router+fileExtension+fileEnding;
+    }
 
         return false;
     });
