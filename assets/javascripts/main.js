99ff551722448dbad33854d37123941f1777b12e
do9xe <h.l.lueth@gmail.com>
2015-05-17 12:41:48 +0200

    remove vendor-option, add 0.7-rc1

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,51 +1,6 @@
 $(document).ready(function() {
-	$('#manufacturer').on('change', function() {
-		selected=this.value;
-		first=[];
-		$("#manufacturer option").each(function() {
-			o=$(this).val();
-			if(o != '-1'){
-				if(o != selected && selected != '-1'){
-					$("#download-form-router option[value^='" + o + "-']").hide();
-				} else {
-					$("#download-form-router option[value^='" + o + "-']").show();
-				}
-			}
-		});
-		if(selected == 'tp-link'){
-			defaultSel=selected + "-tl-wr841n-nd-v9";
-			$("#download-form-router").val(defaultSel);
-		} else {
-			$("#download-form-router").val('-1');
-		}
-		$("#download-form-router").focus();
-		$("#download-form-router").select();
-	});
-	
-	$('#branch').on('change', function() {
-		updateButton()
-	});
-	
-	$('#download-form-type').on('change', function() {
-		updateButton()
-	});
-	
-	function updateButton(){
-		if($('#branch').val() == "1"){
-			button_html="Experimental";
-		} else {
-			button_html="Stable"
-		}
-		
-		if($('#download-form-type').val() == "0"){
-			button_html+=" Neuinstallation";
-		} else {
-			button_html+=" Update";
-		}
-		$("#download-button").html(button_html + " Herunterladen");
-	}
 
-	$('#download-form').submit(function( event ) {
+    $('#download-form').submit(function( event ) {
         event.preventDefault();
 
         var type = '',
@@ -68,7 +23,7 @@ $(document).ready(function() {
                 fileExtension = '-sysupgrade';
                 break;
             default:
-				alert('error')
+                alert('error')
         }
 
             switch ($('#branch').val()) {
@@ -80,12 +35,16 @@ $(document).ready(function() {
                     branchdir = 'experimental';
                     vnumber = '0.7-exp150420';
                     break;
-                default:
+                case '2':
+            branchdir = '07-rc1';
+            vnumber = '0.7';
+            break;
+        default:
                     branchdir = 'stable';
                     vnumber = '0.6';
             }
 
-	if ((router == "netgear-wndr3700" || router == "netgear-wndr3700" || router == "netgear-wndr3800" || 
+    if ((router == "netgear-wndr3700" || router == "netgear-wndr3700" || router == "netgear-wndr3800" || 
         router == "buffalo-wzr-hp-ag300h-wzr-600dhp" || router == "buffalo-wzr-hp-g450h" || 
         router == "d-link-dir-615-rev-c1" || router == "gl-inet-6408a-v1" || router == "gl-inet-6416a-v1" || 
         router == "tp-link-tl-mr3220-v2" || router == "tp-link-tl-wa701n-nd-v1" || 
@@ -97,7 +56,7 @@ $(document).ready(function() {
     {
         window.alert('Der ausgewählte Router ist in dieser Version noch nicht verfügbar!');
     }
-	else if(router === '-1') {
+    else if(router === '-1') {
         window.alert('Bitte wähle eine Router aus.');
     } else {
         window.location.href = '/firmware/'+branchdir+'/'+type+'/gluon-'+site_code+'-'+vnumber+'-'+router+fileExtension+fileEnding;
